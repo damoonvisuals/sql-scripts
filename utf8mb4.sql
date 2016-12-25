6844f55281465fc73bfa60aa9f3aa9d6993a9ad8
Dasun Wang <damoon.sun@gmail.com>
2016-12-24 17:55:00 -0800

    First commit

---
+++ utf8mb4.sql
@@ -0,0 +1,4 @@
+SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci;
+SHOW VARIABLES WHERE Variable_name LIKE 'character\_set\_%' OR Variable_name LIKE 'collation%';
+
+-- Use this option --default-character-set=utf8mb4
