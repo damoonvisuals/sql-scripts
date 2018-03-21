ae8d753c7ead10e88ff8b5137b240c01641283c0
Dasun Wang <damoon.sun@gmail.com>
2018-03-21 00:50:37 -0700

    Add utf8mb4 emoji SQL commands

--- utf8mb4.sql
+++ utf8mb4.sql
@@ -1,4 +1,12 @@
+-- Set utf8mb4 charset with utf8mb4_unicode_ci collation
 SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Verify Settings
 SHOW VARIABLES WHERE Variable_name LIKE 'character\_set\_%' OR Variable_name LIKE 'collation%';
 
 -- Use this option --default-character-set=utf8mb4
+
+-- Create database with charset and collation
+CREATE DATABASE database1
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_unicode_ci;
